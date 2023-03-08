3ed845767e42a41efea723c4f6904343d5026525
nazarene <genarene96@gmail.com>
2023-03-08 08:28:46 -0800

    bug fix2

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,7 @@
 for i in range((10)+1):
     for j in range(10):
         if(i<j):
-            print(f"{i}{j}".format(i,j), end=", ")
+            if(f"{i}{j}" == "89"):
+                print(f"{i}{j}".format(i,j))
+            else:
+                print(f"{i}{j}".format(i,j), end=", ")
