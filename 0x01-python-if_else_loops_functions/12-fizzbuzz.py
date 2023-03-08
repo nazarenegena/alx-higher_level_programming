92ad4c8b7a9d21bc05dc97829a20f5577e5ccde4
nazarene <genarene96@gmail.com>
2023-03-08 11:14:27 -0800

    bug fix2

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(100):
-        if(i%3==0 and i%5==0):
-            print("Fizz", end='')
-        elif(i%5==0):
-            print("Buzz", end='')
-        elif(i%3==0):
-            print("FizzBuzz", end='')
+    for i in range(1, 101):
+        result = ""
+        if(i % 3 == 0):
+            result += "Fizz"
+        if(i % 5 == 0):
+            result += "Buzz"
+        print("{}".format(result) or "{}".format(i), end='')
+        print(" ", end='')
