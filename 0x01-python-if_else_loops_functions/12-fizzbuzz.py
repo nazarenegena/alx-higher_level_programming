69962e12990753e3ffe7b35375d982e0ed819ce7
nazarene <genarene96@gmail.com>
2023-03-08 11:06:42 -0800

    bug fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,8 +2,8 @@
 def fizzbuzz():
     for i in range(100):
         if(i%3==0 and i%5==0):
-            print("Fizz")
+            print("Fizz", end='')
         elif(i%5==0):
-            print("Buzz")
+            print("Buzz", end='')
         elif(i%3==0):
-            print("FizzBuzz")
+            print("FizzBuzz", end='')
