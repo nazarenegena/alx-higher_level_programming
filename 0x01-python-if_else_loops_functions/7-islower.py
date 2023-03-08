b96bcee4f4c40d13b5ee89cb1a76a76a9d961456
nazarene <genarene96@gmail.com>
2023-03-08 09:19:01 -0800

    7islowerbug fix

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-     if (ord(c) >=97):
-          print(f"{c} is lower".format(c))
-     else:
-         print(f"{c} is upper".format(c))
+    return ord(c) in range(97, 123)
