5d5e1686f635aea0ec2df572d9c96f24aa5d26b7
nazarene <genarene96@gmail.com>
2023-03-09 08:54:25 -0800

    bug fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+if __name__ == '__main__':
+    import sys
+    s = 0
+    for i in range(1, len(sys.argv)):
+        s += int(sys.argv[i])
+    print(s)
