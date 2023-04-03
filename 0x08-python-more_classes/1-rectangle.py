bb83bd6a286c4bd820704d9c9d8eeba392dc14f7
nazarene <genarene96@gmail.com>
2023-04-03 09:49:17 -0700

    added attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
      # width getter
-    #@property
+    @property
     def width(self):
         return self.__width
 
     # width setter
-    #@width.setter
+    @width.setter
     def width(self, value):
-        if not isinstance(self.__width, int):
+        if type(value)is not int:
             raise TypeError("width must be an integer")
-        if(self.__width) < 0:
+        if(value)< 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
         # height getter
-
+    @property    
     def height(self):
         return self.__height
 
         #height setter
-
+    @height.setter    
     def height(self, value):
-        if not isinstance(self.__height, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        if(self.__width) < 0:
+        if(value) < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
