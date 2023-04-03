2cd805fdb01ce64b6d5597fbaa618087f1f70c95
nazarene <genarene96@gmail.com>
2023-04-03 09:37:56 -0700

    rearranged methods

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,31 +5,29 @@ class Rectangle:
         self.__height = height
 
      # width getter
+    #@property
     def width(self):
         return self.__width
 
     # width setter
+    #@width.setter
     def width(self, value):
-        self.__width = value
-
-     @staticmethod
-    def widthDataType():
         if not isinstance(self.__width, int):
             raise TypeError("width must be an integer")
         if(self.__width) < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
         # height getter
+
     def height(self):
         return self.__height
 
         #height setter
-    def height(self, value):
-         self.__height = value
 
-    @staticmethod
-    def heightDataType():
+    def height(self, value):
         if not isinstance(self.__height, int):
             raise TypeError("height must be an integer")
         if(self.__width) < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
