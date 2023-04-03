1bad045bf768c2c1f8b77222a8c7e84f87d59355
nazarene <genarene96@gmail.com>
2023-04-03 12:29:39 -0700

    7rectangle file

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+class Rectangle:
+    
+    def __init__(self, width=0, height=0, number_of_instances=0, print_symbol='#'):
+        self.width = width
+        self.height = height
+        self.number_of_instances += number_of_instances 
+        self.print_symbol = print_symbol
+        
+    def __str__(self):
+        return self.print_symbol
+        
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+    
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+          
+   
+         
+    def area(self):
+        return self.__width * self.__height
+    
+    def perimeter(self):
+        perim = (self.__height + self.__width)*2
+        if(self.__width == 0 or self.__height == 0):
+            perim = 0
+        return perim
+        
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ''
+        rectang = ''
+        for i in range(self.__height):
+            rectang += '#' * self.__width
+            if i != self.__height - 1:
+                rectang += '\n'
+        return rectang
+        
+    def __repr__(self):
+         return eval((f"Rectangle({self.width}, {self.height})"))
+         
+    def __del__(self):
+        self.number_of_instances += number_of_instances
+        print("Bye rectangle...")
+
+   
