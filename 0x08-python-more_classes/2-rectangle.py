0c513c52ae566769d0fd77fda709f167ba760cf6
nazarene <genarene96@gmail.com>
2023-04-03 15:42:41 -0700

    new changes

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,49 +5,39 @@ Defining my class Rectangle
 
 
 class Rectangle:
-    def __init__(self, width=0, height=0):
+    """Represent a rectangle."""
 
-         """Initializing a new Rectangle instance
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
         Args:
-            width (int): is the width of the rectangle.
-            height (int): is the height of the rectangle.
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Accessor of the rectangle's width."""
+        """Get/set the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Accessor of the rectangle's length"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
+        """Get/set the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
-
-    def area(self):
-        return self.__width * self.__height
-
-    def perimeter(self):
-        perim = (self.__height + self.__width)*2
-        if(self.__width == 0 or self.__height == 0):
-            perim = 0
-        return perim
+        self.__height = value
