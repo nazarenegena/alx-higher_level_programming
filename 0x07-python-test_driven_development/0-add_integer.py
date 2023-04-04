e5fc0c2fda368bedae90e48df2b9596774dc297a
nazarene <genarene96@gmail.com>
2023-04-04 07:25:54 -0700

    test files added

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
     """ function that adds two intergers"""
-    if isinstance(a, (int,float)):
-        if isinstance(b, (int, float)):
-            return b
-        else:
-            raise TypeError('b must be an integer')
-        return a
-    else:
+    if not isinstance(a, (int,float)):
         raise TypeError('a must be an integer')
+    if not isinstance(b,(int,float)):
+        raise TypeError('b must be an integer')
         
     """ check if they are floats"""
     if isinstance(a, float):
         a=int(a)
-        if isinstance(b, float):
-            b=int(b)
-            return b
-        return a + b
+    if isinstance(b, float):
+        b=int(b)
+    return a + b
