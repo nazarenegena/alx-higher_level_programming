8f2432acbd1af1a4391805f099952bd5705a58bb
nazarene <genarene96@gmail.com>
2023-04-11 10:14:05 -0700

    shortened code

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def lookup(obj):
-    """Getting  a list of all attributes & methods of the object"""
-    attributes_and_methods_lists = dir(obj)
-
     """Returning the filtered list"""
-    return attributes_and_methods_lists
+    return dir(obj)
