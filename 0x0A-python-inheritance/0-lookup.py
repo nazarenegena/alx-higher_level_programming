ebc72cfffc426d8e1cd2b377bcc3a5c40a4ce229
nazarene <genarene96@gmail.com>
2023-04-11 10:08:24 -0700

    bug fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,8 +3,5 @@ def lookup(obj):
     # Getting  a list of all attributes & methods of the object
     attributes_and_methods_lists = dir(obj)
 
-    # Filtering out any private attributes or methods
-    attributes_and_methods_lists = [attr for attr in attributes_and_methods_lists if not attr.startswith('_')]
-
     # Returning the filtered list
     return attributes_and_methods_lists
