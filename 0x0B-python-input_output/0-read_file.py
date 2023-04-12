1164a7b4cb730de4d62dc7ca4d131b09e6677a03
nazarene <genarene96@gmail.com>
2023-04-12 06:48:49 -0700

    readme file and read file added

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Read file function"""
+
+def read_file(filename=""):
+
+    """ with statement used to ensure the file is properly closed after it has been read"""
+
+    """ encoding parameter is set to utf-8"""
+
+    with open(filename, 'r', encoding='utf-8') as f:
+
+        """ read method to read the file"""
+        file_contents = f.read()
+
+        """print the file contents"""
+        print(file_contents)
