e6cf69289f4dc128bd00a9f07ec69e73c60ed7c6
nazarenegena <genarene96@gmail.com>
2023-04-17 19:34:07 -0700

    bug fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,58 +1,68 @@
 #!/usr/bin/python3
+""" inheriting from Rectangle class"""
 
 from models.rectangle import Rectangle
 
-""" inheriting from Rectangle class"""
 
 class Square(Rectangle):
+    """ the Square class """
+
     def __init__(self, size, x=0, y=0, id=None):
+
+        """ constructor of the square class"""
+
         super().__init__(size, size, x, y, id)
+
+        self.size = size
 
     @property
     def size(self):
+
+        """ Defining  the size attribute """
+        """ getter method """
+
         return self.width
-    
+
     @size.setter
     def size(self, value):
+
+        """ Defining the size behavior """
+        """ the setter value"""
+
         self.width = value
+
         self.height = value
 
-    """assigning attributes to key value pairs"""
-    
+    def __str__(self):
+        """ Setting standard values for print """
+
+        si_id = "({}) ".format(self.id)
+
+        si_xy = "{}/{} - ".format(self.x, self.y)
+
+        si_size = "{}".format(self.size)
+
+        return "[Square] " + si_id + si_xy + si_size
+
     def update(self, *args, **kwargs):
-        
-        """ args initiates the attributes"""
-        """ the kwargs : is the attribute of key value pairs"""
 
-        if args and len(args) != 0:
-            j = 0
-            for arg in args:
-                if j == 0:
-                    if arg is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif j == 1:
-                    self.size = arg
-                elif j == 2:
-                    self.x = arg
-                elif j == 3:
-                    self.y = arg
-                j += 1
+        """ Updating the functions attributes """
 
-        elif kwargs and len(kwargs) != 0:
-            for h, q in kwargs.items():
-                if h == "id":
-                    if q is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = q
-                elif h == "size":
-                    self.size = q
-                elif h == "x":
-                    self.x = q
-                elif h == "y":
-                    self.y = q
+        if args is not None and len(args) > 0:
 
-    
-   
+            att = ["id", "size", "x", "y"]
+
+            for j in range(len(args)):
+                setattr(self, att[j], args[j])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ this returns a dictionary representation """
+        k = {
+            'id': self.id, 'size': self.size,
+            'x': self.x, 'y': self.y
+        }
+
+        return k
