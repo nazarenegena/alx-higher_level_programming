95d58c3de00de1df9ec7ac18c6097683f1c81779
nazarene <genarene96@gmail.com>
2023-05-17 03:34:50 -0700

    task 6 & 7 added

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+--script that creates the database 
+--db hbtn_0d_usa & the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states
+(`id` INT AUTO_INCREMENT, `name` VARCHAR(256) NOT NULL, PRIMARY KEY(`id`));
