ed228d94e438986d53e64c4d577afec41c4a22d2
nazarene <genarene96@gmail.com>
2023-05-17 04:56:20 -0700

    task 15 bugfix

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,4 @@
---a script that lists all the comedy shows in the database 
+--a script that lists all the comedyshows in the database
 SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
