71329a18c5e816ccf7345ed436565f1d8f9c7aa3
nazarene <genarene96@gmail.com>
2023-05-17 04:05:49 -0700

    task 11 & 12

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+--script that lists all the shows
+--contains the database hbtn_0d_tvshows
+SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
