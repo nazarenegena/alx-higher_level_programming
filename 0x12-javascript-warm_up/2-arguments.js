6a1e3045b6d807f04a4ea1a29c683996aa77e2f1
nazarenegenaa <genarene96@gmail.com>
2023-06-12 12:01:58 +0000

    task 2 bug fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
-const count = process.argv.length;
-console.log(count === 0 ? 'No argument' : count === 1 ? 'Argument found' : 'Arguments found');
+const args = process.argv.slice(2);
+const numArgs = args.length;
+if (numArgs === 0) {
+  console.log('No argument');
+} else if (numArgs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
