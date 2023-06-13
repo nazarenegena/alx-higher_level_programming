974bd5cda521d07d70c9d39a48572bec38790094
nazarenegenaa <genarene96@gmail.com>
2023-06-13 14:12:38 +0000

    4 rectangle update

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // creates empty object if width / height is not positive int
+      this.width = 0;
+      this.height = 0;
+    }
+  }
+
+    print() {
+        for (let i = 0; i < this.height; i++) {
+            let stringToDisplay = '';
+            for (let j = 0; j < this.width; j++) {
+                stringToDisplay += 'X'
+            }
+
+            console.log(stringToDisplay);
+        }
+
+    }
+
+  rotate() {
+    let tempWidth = this.width;
+    this.width = this.height;
+    this.height = tempWidth;
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
