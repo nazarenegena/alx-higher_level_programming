43b19039c0f8c3b0e888e90b22bfb23fdc08ca61
nazarenegenaa <genarene96@gmail.com>
2023-06-13 14:41:30 +0000

    bug fix

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@ exports.nbOccurences = function (list, searchElement) {
   let countOfOccurrences = 0;
   for (let i = 0; i < list.length; i++) {
     if (searchElement === list[i]) {
-      countOfOccurrences+1;
+      countOfOccurrences++;
     }
   }
   return countOfOccurrences;
