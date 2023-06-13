01398ac0929be27547932d29be850c2493a63b27
nazarenegenaa <genarene96@gmail.com>
2023-06-13 12:08:18 +0000

    3 rectangle updated

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+class Rectangle {
+
+    constructor(w, h) { 
+
+        if (w > 0 && h > 0) {
+
+             this.width = w;
+             this.height = h;
+        } 
+    }
+    print() {
+        let stringToDisplay = '';
+        for (let i = 0; i < this.height; i++){
+            for (let j = 0; j < this.width; j++){
+                stringToDisplay += 'X'
+            }
+                stringToDisplay += "\n"
+        }
+
+        console.log(stringToDisplay);
+    }
+
+}
+
+module.exports = Rectangle;
