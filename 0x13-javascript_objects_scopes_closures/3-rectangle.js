dd4651c082e5364a54ef619aba423cba1925d71f
nazarenegenaa <genarene96@gmail.com>
2023-06-13 12:31:56 +0000

    bug fix1

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -10,17 +10,17 @@ class Rectangle {
         } 
     }
     print() {
-        let stringToDisplay = '';
         for (let i = 0; i < this.height; i++){
+            let stringToDisplay = '';
             for (let j = 0; j < this.width; j++){
                 stringToDisplay += 'X'
             }
-                stringToDisplay += "\n"
+
+            console.log(stringToDisplay);
         }
 
-        return stringToDisplay;
+        
     }
-
 }
 
 module.exports = Rectangle;
