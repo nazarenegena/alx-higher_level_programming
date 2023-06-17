ff771fd5fea707beba754e4f839d872e037b7175
nazarenegena <genarene96@gmail.com>
2023-06-17 04:16:43 -0700

    list items starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" list all states starting with letter N
+from database hbtn_0e_0_usa using MySQLdb """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cursor = database.cursor()
+    cursor.execute('''SELECT * FROM states WHERE name LIKE 'N%' ''')
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    database.close()
