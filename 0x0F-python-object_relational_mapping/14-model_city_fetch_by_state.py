c2bff46e6765a38c58768f28e56d1f0d660c017d
nazarenegena <genarene96@gmail.com>
2023-06-17 07:05:11 -0700

    list cities filtered by state

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" print cities filtered by state
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for city in (session.query(State.name, City.id, City.name)
+                     .filter(State.id == City.state_id)):
+        print(city[0] + ": (" + str(city[1]) + ") " + city[2])
