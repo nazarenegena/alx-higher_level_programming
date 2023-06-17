0f954de8573cb075f6095807b526b10da86f885c
nazarenegena <genarene96@gmail.com>
2023-06-17 05:08:08 -0700

    fixed the display issue

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     database = MySQLdb.connect(host="localhost", user=sys.argv[1],
                                passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = database.cursor()
-    cursor.execute("SELECT * FROM cities")
+    cursor.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
     cities = cursor.fetchall()
     for city in cities:
         print(city)
