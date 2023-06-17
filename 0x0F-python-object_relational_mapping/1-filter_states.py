53235698394e568f713ff094285ea3bf04465117
nazarenegena <genarene96@gmail.com>
2023-06-17 04:20:09 -0700

    added order by

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     database = MySQLdb.connect(host="localhost", user=sys.argv[1],
                                passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = database.cursor()
-    cursor.execute('''SELECT * FROM states WHERE name LIKE 'N%' ''')
+    cursor.execute('''SELECT * FROM states WHERE name LIKE 'N%'
+                      ORDER BY states.id ''')
     states = cursor.fetchall()
     for state in states:
         print(state)
