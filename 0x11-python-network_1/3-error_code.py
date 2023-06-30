926189b39751d02e409bf74a42aa4fda2043ddfb
nazarenegena <genarene96@gmail.com>
2023-06-30 05:25:59 -0700

    configured r variable

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,5 +10,5 @@ from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    res = requests.get("https://api.github.com/user", auth=auth)
-    print(res.json().get("id"))
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
