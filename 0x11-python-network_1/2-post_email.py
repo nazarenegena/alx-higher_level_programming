aef2999078ccca4b1ae3631f65a11d5efcd9b7b2
nazarenegena <genarene96@gmail.com>
2023-06-30 05:20:36 -0700

    task 2 added

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" the script entails:
+- a URL requirement
+- a POST request is sent to the URL
+- email is a parameter
+"""
+
+
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
