37ed6d9243ec2846f21994b5999e672aa5527665
nazarenegena <genarene96@gmail.com>
2023-07-25 12:00:18 +0000

    task 1 done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,5 @@
 #!/usr/bin/node
-import * as fs from 'node:fs';
-import * as fs from 'node:fs/promises';
-// writing a script that reads & prints file contents.
-const fs = require('fs').promises;
-
-let filePath = 'file_path.txt';
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-            console.error(`Error reading the file: ${err}`);
-    } else
-    {
-       console.log(data) 
-        }
-})
+const fsFile = require('fs');
+fsFile.readFile(process.argv[2], 'utf8', function (error, data) {
+  console.log(error || data);
+});
